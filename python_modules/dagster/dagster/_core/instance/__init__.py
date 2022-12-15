cf975db1731301149ed45891c8993eaeb4fece88
Sean Mackesey <s.mackesey@gmail.com>
2022-12-15 05:25:43 -0500

    stale schedule

--- python_modules/dagster/dagster/_core/instance/__init__.py
+++ python_modules/dagster/dagster/_core/instance/__init__.py
@@ -96,6 +96,7 @@ if TYPE_CHECKING:
     from dagster._core.execution.plan.plan import ExecutionPlan
     from dagster._core.execution.plan.resume_retry import ReexecutionStrategy
     from dagster._core.execution.stats import RunStepKeyStatsSnapshot
+    from dagster._core.definitions.job_definition import JobDefinition
     from dagster._core.host_representation import (
         ExternalPipeline,
         ExternalSensor,
@@ -2230,3 +2231,7 @@ class DagsterInstance:
             materialization = next(iter(materializations), None)
 
         return materialization or observation
+
+    def get_stale_assets(self, job_def: JobDefinition) -> AbstractSet[AssetKey]:
+        all_keys = job_def.asset_layer.asset_keys
+
